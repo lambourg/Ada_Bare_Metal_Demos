6af173b3143344e1b684b84bc01531bcbef2fa54
Tristan Gingold <gingold@adacore.com>
2016-11-15 16:04:46 +0100

    rpi2-mmc: add info and speed test.

--- rpi2-mmc/src/sdcard_demo.adb
+++ rpi2-mmc/src/sdcard_demo.adb
@@ -24,6 +24,7 @@
 with Ada.Unchecked_Conversion;
 with Interfaces;                 use Interfaces;
 
+with Ada.Real_Time;
 with Ada.Text_IO;                use Ada.Text_IO;
 
 with Filesystem;                 use Filesystem;
@@ -31,8 +32,12 @@ with Filesystem.MBR;             use Filesystem.MBR;
 with Filesystem.FAT;             use Filesystem.FAT;
 with Filesystem.VFS;             use Filesystem.VFS;
 
-with HAL.SDCard; use HAL.SDCard;
-with MMC; use MMC;
+with HAL;                        use HAL;
+with HAL.Block_Drivers;
+with HAL.SDCard;                 use HAL.SDCard;
+with MMC;                        use MMC;
+
+with Hex_Images; use Hex_Images;
 
 procedure SDCard_Demo
 is
@@ -73,41 +78,13 @@ is
       Close (Dir);
    end Display_Current_Dir;
 
-   function Setup return Boolean is
+   SD_Card_Info  : Card_Information;
+
+   procedure Disp_Capacity is
       Units : constant array (Natural range <>) of Character :=
         (' ', 'k', 'M', 'G', 'T');
       Capacity      : Unsigned_64;
-      SD_Card_Info  : Card_Information;
-      SD_Status     : SD_Error;
    begin
-      Initialize (EMMC_Driver, SD_Card_Info, SD_Status);
-      if SD_Status /= OK then
-         Put_Line ("Card initialization failed");
-         return False;
-      end if;
-
-      case SD_Card_Info.Card_Type is
-         when STD_Capacity_SD_Card_V1_1 =>
-            Put_Line ("SD card v1.1");
-         when STD_Capacity_SD_Card_V2_0 =>
-            Put_Line ("SD card v2.0");
-         when High_Capacity_SD_Card =>
-            Put_Line ("SD card high capacity");
-            Put ("Device size:");
-            Put (Unsigned_32'Image (SD_Card_Info.SD_CSD.Device_Size));
-            New_Line;
-         when Multimedia_Card =>
-            Put_Line ("MMC");
-         when High_Speed_Multimedia_Card =>
-            Put_Line ("High speed MMC");
-         when High_Capacity_Mmc_Card =>
-            Put_Line ("High speed MMC");
-         when Secure_Digital_Io_Card =>
-            Put_Line ("SDIO card");
-         when Secure_Digital_Io_Combo_Card =>
-            Put_Line ("SDIO combo card");
-      end case;
-
       --  Dump general info about the SD-card
       Capacity := SD_Card_Info.Card_Capacity;
 
@@ -123,6 +100,145 @@ is
             Capacity := Capacity / 1000;
          end if;
       end loop;
+   end Disp_Capacity;
+
+   procedure Disp_Info is
+   begin
+      case SD_Card_Info.Card_Type is
+         when STD_Capacity_SD_Card_V1_1 =>
+            Put_Line ("SD card v1.1");
+         when STD_Capacity_SD_Card_V2_0 =>
+            Put_Line ("SD card v2.0");
+         when High_Capacity_SD_Card =>
+            Put_Line ("SD card high capacity");
+         when Multimedia_Card =>
+            Put_Line ("MMC");
+         when High_Speed_Multimedia_Card =>
+            Put_Line ("High speed MMC");
+         when High_Capacity_Mmc_Card =>
+            Put_Line ("High speed MMC");
+         when Secure_Digital_Io_Card =>
+            Put_Line ("SDIO card");
+         when Secure_Digital_Io_Combo_Card =>
+            Put_Line ("SDIO combo card");
+      end case;
+   end Disp_Info;
+
+   procedure Disp_CID (CID: Card_Identification_Data_Register) is
+   begin
+      Put ("MID: ");
+      Put (Hex2 (CID.Manufacturer_ID));
+      Put (", OID: ");
+      Put (CID.OEM_Application_ID);
+      Put (", Name: ");
+      Put (CID.Product_Name);
+      Put (", Rev: ");
+      Put (UInt4'Image (CID.Product_Revision.Major));
+      Put (UInt4'Image (CID.Product_Revision.Minor));
+      Put (", S/N: ");
+      Put (Hex8 (CID.Product_Serial_Number));
+      Put (", Date: ");
+      Put (Manufacturing_Year'Image (CID.Manufacturing_Date.Year));
+      Put (Manufacturing_Month'Image (CID.Manufacturing_Date.Month));
+      New_Line;
+   end Disp_CID;
+
+   procedure Disp_CSD (CSD: Card_Specific_Data_Register) is
+   begin
+      Put ("CSD structure V");
+      Put (Byte'Image (CSD.CSD_Structure + 1));
+      New_Line;
+      Put (" Tran_Speed: ");
+      case Shift_Right (CSD.Max_Data_Transfer_Rate, 3) and 15 is
+         when 16#1# => Put ("1.0");
+         when 16#2# => Put ("1.2");
+         when 16#3# => Put ("1.3");
+         when 16#4# => Put ("1.5");
+         when 16#5# => Put ("2.0");
+         when 16#6# => Put ("2.5");
+         when 16#7# => Put ("3.0");
+         when 16#8# => Put ("3.5");
+         when 16#9# => Put ("4.0");
+         when 16#a# => Put ("4.5");
+         when 16#b# => Put ("5.0");
+         when 16#c# => Put ("5.5");
+         when 16#d# => Put ("6.0");
+         when 16#e# => Put ("7.0");
+         when 16#f# => Put ("8.0");
+         when others => Put ("?");
+      end case;
+      case CSD.Max_Data_Transfer_Rate and 7 is
+         when 0 => Put (" * 100kb/s");
+         when 1 => Put (" * 1Mb/s");
+         when 2 => Put (" * 10Mb/s");
+         when 3 => Put (" * 100Mb/s");
+         when others => Put (" * ???kb/s");
+      end case;
+      Put (", Card classes:");
+      for I in reverse 1 .. 11 loop
+         if (Shift_Right (CSD.Card_Command_Class, I) and 1) /= 0 then
+            Put (Natural'Image (I));
+         end if;
+      end loop;
+      New_Line;
+   end Disp_CSD;
+
+   procedure Disp_SCR (SCR : SDCard_Configuration_Register) is
+   begin
+      Put ("SCR structure #");
+      Put (Byte'Image (SCR.SCR_Structure));
+      Put (", Spec: ");
+      case SCR.SD_Spec is
+         when 0 => Put ("V1.0");
+         when 1 => Put ("V1.10");
+         when 2 =>
+            if not SCR.SD_Spec4 then
+               if not SCR.SD_Spec3 then
+                  Put ("V2.00");
+               else
+                  Put ("V3.0X");
+               end if;
+            else
+               if SCR.SD_Spec3 then
+                  Put ("V4.XX");
+               else
+                  Put ("V3??");
+               end if;
+            end if;
+         when others =>
+            Put ("V??");
+      end case;
+      New_Line;
+      Put (" Data_Stat_afer_erase:");
+      Put (Byte'Image (SCR.Data_Stat_After_Erase));
+      Put (", SD_Security:");
+      Put (Byte'Image (SCR.SD_Security));
+      New_Line;
+      Put (" Bus width:");
+      if (SCR.SD_Bus_Widths and 1) /= 0 then
+         Put (" 1bit");
+      end if;
+      if (SCR.SD_Bus_Widths and 4) /= 0 then
+         Put (" 4bit");
+      end if;
+      if (SCR.SD_Bus_Widths and 10) /= 0 then
+         Put (" ??");
+      end if;
+      Put (", EX_Security:");
+      Put (Byte'Image (SCR.EX_Security));
+      Put (", CMD_Support:");
+      Put (Byte'Image (SCR.CMD_Support));
+      New_Line;
+   end Disp_SCR;
+
+   function Setup return Boolean is
+      SD_Status     : SD_Error;
+   begin
+      Initialize (EMMC_Driver, SD_Card_Info, SD_Status);
+      if SD_Status /= OK then
+         Put_Line ("Card initialization failed");
+         return False;
+      end if;
 
       return True;
    end Setup;
@@ -149,11 +265,55 @@ is
       end if;
    end Do_List;
 
+   procedure Do_Info is
+   begin
+      if not Setup then
+         return;
+      end if;
+
+      Disp_CID (SD_Card_Info.SD_CID);
+      New_Line;
+      Disp_SCR (SD_Card_Info.SD_SCR);
+      New_Line;
+      Disp_CSD (SD_Card_Info.SD_CSD);
+      New_Line;
+      Disp_Info;
+      Disp_Capacity;
+   end Do_Info;
+
+   procedure Do_Speed is
+      use Ada.Real_Time;
+      Start, Stop : Time;
+      Elaps : Time_Span;
+      Data : HAL.Block_Drivers.Block (0 .. 511);
+      Ms : Natural;
+   begin
+      if not Setup then
+         return;
+      end if;
+
+      Start := Clock;
+      for Num in Unsigned_64 range 0 .. 2048 loop
+         if not Read (EMMC_Driver, Num, Data) then
+            Put_Line ("Read failure");
+            exit;
+         end if;
+      end loop;
+      Stop := Clock;
+      Elaps := Stop - Start;
+      Put ("Time to read 1MB:");
+      Ms := Elaps / Milliseconds (1);
+      Put (Natural'Image (Ms));
+      Put_Line (" ms");
+   end Do_Speed;
+
    C : Character;
 begin
    loop
       Put_Line ("Menu:");
       Put_Line ("l: list all files");
+      Put_Line ("i: disp card info");
+      Put_Line ("s: speed test");
       Put_Line ("q: quit");
       loop
          Put ("Your choice ? ");
@@ -165,6 +325,12 @@ begin
                return;
             when 'l' =>
                Do_List;
+               exit;
+            when 'i' =>
+               Do_Info;
+               exit;
+            when 's' =>
+               Do_Speed;
                exit;
             when others =>
                Put_Line ("Unknown choice");
