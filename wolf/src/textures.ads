7eef905cc07df90370e88767c76c060f21ae4a49
Jerome Lambourg <lambourg@adacore.com>
2017-02-25 12:28:19 +0100

    Update the wolf textures
    * bump the textures size to 128x128 pixels
    * nicer looking ones.
    * add the dark version, all textures are now in flash (lifts the limit on
      available coupled memory)

--- wolf/src/textures.ads
+++ wolf/src/textures.ads
@@ -25,7 +25,7 @@ with Interfaces; use Interfaces;
 
 package Textures is
 
-   type Texture is array (0 .. 63, 0 .. 63) of Unsigned_16
+   type Texture is array (0 .. 127, 0 .. 127) of Unsigned_16
      with Component_Size => 16;
 
    type Texture_Access is access constant Texture;
